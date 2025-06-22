470117939c45c4411e1b092ad25803392542d469
NigelXheri <nigelxheri@gmail.com>
2025-06-22 23:47:16 +0200

    Qartesim i koncepteve te operatoreve

--- Summer School/05_Operatoret krahasues dhe logjike.py
+++ Summer School/05_Operatoret krahasues dhe logjike.py
@@ -14,18 +14,28 @@ print(5 == 5)
 # Mosbarazimi(Jo e barabartë)
 print(10 != 5)
 
+print("\n")
 
 # ========== Operatorët logjikë ==========
+
+kushti1 = True
+kushti2 = True
 
 # Operatori 'dhe'
 kushti1 and kushti2  # and është operator që bashkon dy kushte
 # Kështu krijohet një kusht i ri kompleks(i përbërë) i cili plotësohet vetëm kur të dyja kushtet janë të vërteta
+print("kushti1 and kushti2")
+print(kushti1 and kushti2)
 
 # Operatori 'ose'
 kushti1 or kushti2
 # Ky kusht i ri i cili plotësohet kur të paktën njëra nga kushtet është e vërtetë
+print("kushti1 or kushti2")
+print(kushti1 or kushti2)
 
 # Operatori 'not'
 # kthen rezultatin e kundërt të kushtit kompleks brenda kllapave
 not (kushti1 and kushti2)
 # Ky kusht i ri i cili plotësohet vetëm kur kushti kompleks brenda kllapave nuk është i përbërë
+print("not (kushti1 and kushti2)")
+print(not (kushti1 and kushti2))
